534c9ea8da61e33e532eba9e21eb28019a368a3d
isaac diez <isaac.diez@proton.me>
2025-12-10 18:35:44 +0100

    chore(spec): Update app.component spec placeholder [app.component.spec.ts]

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'new-weather-frontend'`, () => {
+  it(`should have as title 'weather-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('new-weather-frontend');
+    expect(app.title).toEqual('weather-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('new-weather-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('weather-frontend app is running!');
   });
 });
