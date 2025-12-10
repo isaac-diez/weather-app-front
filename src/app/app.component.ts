9772258a70e6b740aa3f8c8b2592540910cb38b0
isaac diez <isaac.diez@proton.me>
2025-12-10 18:37:33 +0100

    fix(spec): Update title and remove styleUrls [app.component.ts]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'new-weather-frontend';
+  title = 'weather-frontend';
 }
