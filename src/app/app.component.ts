a872b85bd46c743a0dee0744c4fb2e9e2c1aad8e
isaac diez <isaac.diez@proton.me>
2025-12-21 21:24:36 +0100

    feat(component): added constructor for TranslateService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-frontend';
+  constructor(private translate: TranslateService) {
+
+    this.translate.setDefaultLang('en');
+
+    const browserLang = this.translate.getBrowserLang();
+
+    this.translate.use(browserLang?.match(/es/) ? 'es' : 'en');
+  }
 }
